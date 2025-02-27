cee3af996777669c1f90c4ee2fc593370729365e
HoldMyPiwko <koszolkolukasz@gmail.com>
2025-02-27 16:50:01 +0100

    to jest pojebane

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Point p = new Point();
         p.x = 5.0;
         p.y = 3.0;
-        System.out.println("P="+p);
+        System.out.println("P=" + p);
 
         p.translate(5, -5);
         System.out.println();
@@ -19,8 +19,6 @@ public class Main {
         seg.a.y = 1.0;
         seg.b.x = 5;
         seg.b.y = 4;
-        System.out.println("seg len: "+seg.length());
-
-        }
+        System.out.println("seg len: " + seg.length());
 
     }
--- svg/src/Segment.java
+++ svg/src/Segment.java
@@ -8,3 +8,21 @@ public class Segment {
         return Math.sqrt(pow(a.x-b.x, 2) + pow(a.y-b.y, 2));
     }
 }
+Segment[] tab = new Segment[2];
+tab[0] = seg;
+
+tab[1] = new Segment();
+tab[1].a
+
+
+public static Segment maxSegment(Segment[] arr) {
+    if (arr.length == 0)
+        return null;
+
+    Segment max = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+        if (arr[i].length() > max.length()) {
+            max = arr[i];
+        }
+    }
+}
